fdf801309146bc8f98e6e1395ac90579ff2af03e
Andresouzanl <devals2022@gmail.com>
2026-01-03 22:37:55 -0300

    Primeiro

---
+++ MeuProjeto/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={estilos.container}>
+      <Text style={estilos.texto}>Hello World</Text>
+      <Text>Criado Por DevSouza</Text>
+    </View>
+  );
+}
+
+const estilos = StyleSheet.create({
+  container:{
+    backgroundColor:'lightblue',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  texto: {
+    marginTop: 20,
+    fontSize: 20,
+  },
+});
