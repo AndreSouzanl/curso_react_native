4a0e06560af21f20f7e567c1a5132811ad894d27
Andresouzanl <devals2022@gmail.com>
2026-01-27 22:03:53 -0300

    atualiza

--- AppParati/App.js
+++ AppParati/App.js
@@ -2,9 +2,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import TelaInicial from "./componentes/TelaInicial";
-import TelaRestaurantes from "./componentes/restaurantes";
+import TelaRestaurantes from "./componentes/TelaRestaurantes";
 import TelaPasseios from "./componentes/TelaPasseios";
-import TelaHospedagem from "./componentes/Hospedagem";
+import TelaHospedagem from "./componentes/TelaHospedagem";
 
 const Stack = createStackNavigator()
 
